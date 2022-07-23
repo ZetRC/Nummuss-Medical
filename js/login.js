91821d7cad96696c23501209d50803f006337629
ZetRC <zetrosas555@gmail.com>
2022-07-23 15:35:38 -0500

    caja de seleccion de formulario de registro añadida, formulario de registro para doctor añadido

--- js/login.js
+++ js/login.js
@@ -2,55 +2,72 @@ var login = (function() {
 		
 	var initProperties = function() {
          functions.handleWizard();
+         functions.handleFade();
 	};
 	
 	var functions = {	
         handleWizard : function(){
-                // ------------step-wizard-------------
-$(document).ready(function () {
-    $('.nav-tabs > li a[title]').tooltip();
-    
-    //Wizard
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        $(document).ready(function () {
+            $('.nav-tabs > li a[title]').tooltip();
+            
+            $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
 
-        var target = $(e.target);
-    
-        if (target.parent().hasClass('disabled')) {
-            return false;
+                var target = $(e.target);
+            
+                if (target.parent().hasClass('disabled')) {
+                    return false;
+                }
+            });
+
+            $(".next-step").click(function (e) {
+                var active = $(this).closest('.wizard').find('.nav-tabs li.active');
+                active.next().removeClass('disabled');
+                nextTab(active);
+
+            });
+            $(".prev-step").click(function (e) {
+
+                var active = $(this).closest('.wizard').find('.nav-tabs li.active');
+                prevTab(active);
+
+            });
+        });
+
+        function nextTab(elem) {
+            $(elem).next().find('a[data-toggle="tab"]').click();
         }
-    });
-
-    $(".next-step").click(function (e) {
-
-        var active = $('.wizard .nav-tabs li.active');
-        active.next().removeClass('disabled');
-        nextTab(active);
-
-    });
-    $(".prev-step").click(function (e) {
-
-        var active = $('.wizard .nav-tabs li.active');
-        prevTab(active);
-
-    });
-});
-
-function nextTab(elem) {
-    $(elem).next().find('a[data-toggle="tab"]').click();
-}
-function prevTab(elem) {
-    $(elem).prev().find('a[data-toggle="tab"]').click();
-}
+        function prevTab(elem) {
+            $(elem).prev().find('a[data-toggle="tab"]').click();
+        }
 
 
-$('.nav-tabs').on('click', 'li', function() {
-    $('.nav-tabs li.active').removeClass('active');
-    $(this).addClass('active');
-});
-
-
-
+        $('.nav-tabs').on('click', 'li', function() {
+            $(this).closest('.nav-tabs').find('li.active').removeClass('active');
+            //$('.nav-tabs li.active').removeClass('active');
+            $(this).addClass('active');
+        });
         },
+
+        handleFade : function(){
+            $('.toggle-content').click(()=>{
+                $('.selection-box').fadeOut(200)
+            })
+
+            $('#doctor-selector').click(()=>{
+                $('#formularioDoctor').delay(200).fadeIn(200)
+            })
+
+            $('#paciente-selector').click(()=>{
+                $('#formularioPaciente').delay(200).fadeIn(200)
+            })
+
+            $('.back-selection').click(()=>{
+                $(".selection-box").delay(200).fadeIn(200)
+                $('#formularioDoctor').fadeOut(200)
+                $('#formularioPaciente').fadeOut(200)
+                
+            })
+        }
 	};
 	
 	var initialize = function() {
