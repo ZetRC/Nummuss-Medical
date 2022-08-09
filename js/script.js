41b69610a343ab175c3c69732d596fd2f7955ed1
ZetRC <zetrosas555@gmail.com>
2022-08-09 17:40:28 -0500

    sidebar añadida

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ var homeLayout = (function() {
          functions.toggleableTables();
          functions.initCustomCarousel();
          functions.handleImageZoom();
+         functions.handleSidebar();
+         functions.dropdown();
 	};
 	
 	var functions = {	
@@ -28,7 +30,6 @@ var homeLayout = (function() {
               });
               
               // handles the carousel thumbnails
-              // <img src="https://images.unsplash.com/photo-1532153975070-2e9ab71f1b14?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" class="img-fluid" alt="...">
               $('[id^=carousel-selector-]').click(function() {
                 var id_selector = $(this).attr('id');
                 var id = parseInt( id_selector.substr(id_selector.lastIndexOf('-') + 1) );
@@ -82,7 +83,6 @@ var homeLayout = (function() {
               });
         },
         handleImageZoom : function(){
-          //initiate the plugin and pass the id of the div containing gallery images
           $(".zoom-image").elevateZoom({
             cursor: 'pointer',
             easing : true,
@@ -90,7 +90,41 @@ var homeLayout = (function() {
             imageCrossfade: true,
             loadingIcon: 'https://www.elevateweb.co.uk/spinner.gif'
           });
-        }
+        },
+        handleSidebar : function(){
+          $('.sidebar-element').click(function(){
+            //cambia la seccion actual
+            const target = $(this).attr('data-target')
+            $('.body-section').removeClass('d-block').addClass('hidden')
+            $(target).removeClass('hidden').addClass('d-block')
+            //cambia color del elemento de sidebar seleccionado
+            $('.sidebar-element').removeClass("selected-item")
+            $(this).addClass("selected-item")
+          })
+
+          $("#perfil-selector").click(function(){
+            const target = $(this).attr('data-target')
+            $('.body-section').removeClass('d-block').addClass('hidden')
+            $(target).removeClass('hidden').addClass('d-block')
+            console.log(target)
+          })
+        },
+        dropdown : function(){
+
+          $(".section-header").click(function(){
+            
+            if($(this).parent().find(".section-content").hasClass("active-dropdown")){
+              $("#sidebar").find(".active-dropdown").removeClass("active-dropdown").slideUp(200);
+              $(this).find(".fa-chevron-right").toggleClass("rotate");
+            }else{
+              $("#sidebar").find(".active-dropdown").removeClass("active-dropdown").slideUp(200);
+              $(".fa-chevron-right").removeClass("rotate")
+
+              $(this).find(".fa-chevron-right").addClass("rotate");
+              $(this).parent().find(".section-content").addClass("active-dropdown").slideDown(200);
+            }
+          })
+        },
 	};
 	
 	var initialize = function() {
